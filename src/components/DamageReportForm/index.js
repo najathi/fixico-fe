e88005a9b9d371fa63d2daf95721a7d8281c8d7d
najathi <najathi777@gmail.com>
2023-04-26 15:40:29 +0530

    amend multi-step form

--- src/components/DamageReportForm/index.js
+++ src/components/DamageReportForm/index.js
@@ -8,6 +8,14 @@ export default function DamageReportForm() {
     const { data: vehicles, error } = useSWR('/api/vehicles', fetcher);
     const router = useRouter();
 
+    const [step, setStep] = useState(1);
+    const nextStep = () => {
+        setStep(step + 1);
+    };
+    const prevStep = () => {
+        setStep(step - 1);
+    };
+
     const [models, setModels] = useState(null);
     const [data, setData] = useState({
         vehicle_id: "",
@@ -142,161 +150,228 @@ export default function DamageReportForm() {
         }
     }, [data, error?.errors]);
 
-    return (
-        <div className="flex flex-col">
-            <ul className="steps steps-vertical lg:steps-horizontal mb-4 w-full">
-                <li className="step step-primary">Vehicle details</li>
-                <li className="step">Upload image</li>
-                <li className="step">Contact</li>
-                <li className="step">Summery</li>
-            </ul>
-
-            <form onSubmit={damageReportSubmitHandler}>
-
-                <p className="text-sky-400 underline pb-3">Vehicle details</p>
-                <div className="form-control w-full max-w-md pb-3">
-                    <label className="label">
-                        <span className="label-text">Brand</span>
-                    </label>
-                    {vehicles &&
-                        <select
-                            className="select select-bordered select-sm"
-                            name="vehicle_id"
-                            id="vehicle_id"
-                            value={data.vehicle_id}
-                            onChange={mapToModelFieldHandler}
-                            required
-                        >
-                            {vehicles.map(item => (
-                                <Fragment key={item.id}>
-                                    <option value={item.id}>{item.name}</option>
-                                </Fragment>
-                            ))}
-                        </select>
-                    }
-                </div>
-
-                {data.vehicle_id &&
-                    models && models.length > 0 &&
-                    <div className="form-control w-full max-w-md pb-3">
-                        <label className="label">
-                            <span className="label-text">Model</span>
-                        </label>
-                        <select
-                            className="select select-bordered select-sm"
-                            name="model_id"
-                            id="model_id"
-                            value={data.model_id}
-                            onChange={handleInputChange}
-                            required
-                        >
-                            {models.map(item => (
-                                <Fragment key={item.id}>
-                                    <option value={item.id}>{item.name}</option>
-                                </Fragment>
-                            ))}
-                        </select>
-                    </div>
+    const renderStepOne = () => (
+        <>
+            <p className="text-sky-400 underline pb-3">Vehicle details</p>
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">Brand</span>
+                </label>
+                {vehicles &&
+                    <select
+                        className="select select-bordered select-sm"
+                        name="vehicle_id"
+                        id="vehicle_id"
+                        value={data.vehicle_id}
+                        onChange={mapToModelFieldHandler}
+                        required
+                    >
+                        {vehicles.map(item => (
+                            <Fragment key={item.id}>
+                                <option value={item.id}>{item.name}</option>
+                            </Fragment>
+                        ))}
+                    </select>
                 }
+            </div>
 
-                <p className="text-sky-400 underline py-3">Upload photo's of the damage</p>
+            {data.vehicle_id &&
+                models && models.length > 0 &&
                 <div className="form-control w-full max-w-md pb-3">
                     <label className="label">
-                        <span className="label-text">Photo's of the damage?</span>
+                        <span className="label-text">Model</span>
                     </label>
-                    <input
-                        type="file"
-                        className="file-input file-input-bordered file-input-sm w-full max-w-md"
-                        name="image"
-                        id="image"
-                        onChange={handleInputChange}
-                        required
-                    />
-
-                    {data.isImageLoading && <p>Image is Uploading</p>}
-                </div>
-
-                <div className="form-control w-full max-w-md pb-3">
-                    <label className="label">
-                        <span className="label-text">description?</span>
-                    </label>
-                    <textarea
-                        placeholder="Description"
-                        className="textarea textarea-bordered textarea-sm w-full max-w-md"
-                        name="description"
-                        id="description"
-                        value={data.description}
-                        onChange={handleInputChange}
-                        required
-                    ></textarea>
-                </div>
-
-                <p className="text-sky-400 underline py-3">Customer details</p>
-                <div className="form-control w-full max-w-md pb-3">
-                    <label className="label">
-                        <span className="label-text">Your name?</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Type here"
-                        className="input input-bordered w-full max-w-md input-sm"
-                        name="cName"
-                        id="cName"
-                        value={data.cName}
-                        onChange={handleInputChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-control w-full max-w-md pb-3">
-                    <label className="label">
-                        <span className="label-text">E-Mail Address?</span>
-                    </label>
-                    <input
-                        type="email"
-                        placeholder="Type here"
-                        className="input input-bordered w-full max-w-md input-sm"
-                        name="cEmail"
-                        id="cEmail"
-                        value={data.cEmail}
-                        onChange={handleInputChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-control w-full max-w-md pb-3">
-                    <label className="label">
-                        <span className="label-text">Phone Number?</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Type here"
-                        className="input input-bordered w-full max-w-md input-sm"
-                        name="cPhone"
-                        id="cPhone"
-                        value={data.cPhone}
-                        onChange={handleInputChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-control w-full max-w-md pb-3">
-                    <label className="label">
-                        <span className="label-text">Message?</span>
-                    </label>
-                    <textarea
-                        placeholder="Type here"
-                        className="textarea textarea-bordered textarea-sm w-full max-w-md"
-                        name="cMessage"
-                        id="cMessage"
-                        value={data.cMessage}
+                    <select
+                        className="select select-bordered select-sm"
+                        name="model_id"
+                        id="model_id"
+                        value={data.model_id}
                         onChange={handleInputChange}
                         required
                     >
-                    </textarea>
+                        {models.map(item => (
+                            <Fragment key={item.id}>
+                                <option value={item.id}>{item.name}</option>
+                            </Fragment>
+                        ))}
+                    </select>
                 </div>
+            }
 
-                <button type="submit" className="btn btn-primary" disabled={data.isSubmitting || data.isImageLoading}>Submit</button>
+            <div className="btn-group">
+                <button
+                    type="button"
+                    className="btn btn-active"
+                    disabled={step >= 3}
+                    onClick={nextStep}
+                >
+                    Next
+                </button>
+            </div>
+        </>
+    )
+
+    const renderStepTwo = () => (
+        <>
+            <p className="text-sky-400 underline py-3">Upload photo's of the damage</p>
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">Photo's of the damage?</span>
+                </label>
+                <input
+                    type="file"
+                    className="file-input file-input-bordered file-input-sm w-full max-w-md"
+                    name="image"
+                    id="image"
+                    onChange={handleInputChange}
+                    required
+                />
+
+                {data.isImageLoading && <p>Image is Uploading</p>}
+            </div>
+
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">description?</span>
+                </label>
+                <textarea
+                    placeholder="Description"
+                    className="textarea textarea-bordered textarea-sm w-full max-w-md"
+                    name="description"
+                    id="description"
+                    value={data.description}
+                    onChange={handleInputChange}
+                    required
+                ></textarea>
+            </div>
+
+            <div className="btn-group">
+                <button
+                    type="button"
+                    className="btn"
+                    disabled={step <= 1}
+                    onClick={prevStep}
+                >
+                    Previous
+                </button>
+
+                <button
+                    type="button"
+                    className="btn btn-active"
+                    disabled={step >= 3}
+                    onClick={nextStep}
+                >
+                    Next
+                </button>
+            </div>
+        </>
+    )
+
+    const renderStepThree = () => (
+        <>
+            <p className="text-sky-400 underline py-3">Customer details</p>
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">Your name?</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-md input-sm"
+                    name="cName"
+                    id="cName"
+                    value={data.cName}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">E-Mail Address?</span>
+                </label>
+                <input
+                    type="email"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-md input-sm"
+                    name="cEmail"
+                    id="cEmail"
+                    value={data.cEmail}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">Phone Number?</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-md input-sm"
+                    name="cPhone"
+                    id="cPhone"
+                    value={data.cPhone}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+
+            <div className="form-control w-full max-w-md pb-3">
+                <label className="label">
+                    <span className="label-text">Message?</span>
+                </label>
+                <textarea
+                    placeholder="Type here"
+                    className="textarea textarea-bordered textarea-sm w-full max-w-md"
+                    name="cMessage"
+                    id="cMessage"
+                    value={data.cMessage}
+                    onChange={handleInputChange}
+                    required
+                >
+                </textarea>
+            </div>
+
+            <div className="btn-group">
+                <button
+                    type="button"
+                    className="btn"
+                    disabled={step <= 1}
+                    onClick={prevStep}
+                >
+                    Previous
+                </button>
+            </div>
+
+            <button type="submit" className="btn btn-primary" disabled={data.isSubmitting || data.isImageLoading}>Submit</button>
+        </>
+    )
+
+    const renderFormElements = () => {
+        switch (step) {
+            case 1:
+                return renderStepOne();
+            case 2:
+                return renderStepTwo();
+            case 3:
+                return renderStepThree();
+            default:
+                return null;
+        }
+    }
+
+    return (
+        <div className="flex flex-col">
+            <ul className="steps steps-vertical lg:steps-horizontal mb-4 w-full">
+                <li className={`step ${step === 1 && 'step-primary'}`}>Vehicle details</li>
+                <li className={`step ${step === 2 && 'step-primary'}`}>Upload image</li>
+                <li className={`step ${step === 3 && 'step-primary'}`}>Contact Information</li>
+            </ul>
+
+            <form onSubmit={damageReportSubmitHandler}>
+                {renderFormElements()}
             </form>
         </div>
     )
